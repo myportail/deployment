ccfdd7a4b62356de50dfdd20a889276d2e9200fc
dannyt <cobreti_@hotmail.com>
2019-12-01 17:57:11 -0500

    added overlays to ease deployment

--- kubernetes/authdb/create.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-kubectl apply -f authdb.storage.do.yml --namespace=myportail
-kubectl apply -f authdb.configmap.yml --namespace=myportail
-kubectl apply -f authdb-mariadb.service.yml --namespace=myportail
--- kubernetes/authdb/delete.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-kubectl delete -f authdb-mariadb.service.yml --namespace=myportail
-kubectl delete -f authdb.storage.do.yml --namespace=myportail
-kubectl delete -f authdb.configmap.yml --namespace=myportail
